e336df802a43020145fec22f090acf29c7d75075
Flosus <flosus.mail@gmail.com>
2015-04-22 11:13:41 +0200

    Initial Project Commit
    The initial commit for this project
    
    Signed-off-by: Flosus <flosus.mail@gmail.com>

---
+++ PackIt/PackIt/Program.cs
@@ -0,0 +1,18 @@
+﻿using PackIt.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace PackIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PackEnvironment.Instance.InitArguments(args);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ PackIt/PackIt/Util/PackEnvironment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PackIt.Util
+{
+    internal class PackEnvironment
+    {
+        /// <summary>The private instance.</summary>
+        private static PackEnvironment _Instance;
+        /// <summary>The instance of the package environment.</summary>
+        public static PackEnvironment Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                    _Instance = new PackEnvironment();
+                return _Instance;
+            }
+        }
+
+        /// <summary>Private constructir</summary>
+        private PackEnvironment() { }
+
+        private Dictionary<string, string> parameter = new Dictionary<string, string>();
+
+        public string this[string key]
+        {
+            get
+            {
+                return parameter[key];
+            }
+            set
+            {
+                if (value == null)
+                    parameter.Remove(key);
+                else if (value == string.Empty)
+                    parameter[key] = true.ToString();
+                else
+                    parameter[key] = value;
+            }
+        }
+
+        /// <summary>Initialises the argument strings.</summary>
+        /// <param name="args">The arguments</param>
+        public void InitArguments(string[] args)
+        {
+            // Setting default XML file
+            PackEnvironment.Instance["XML"] = "pack.xml";
+        }
+    }
+}
