c6e16316b4c652b1fc5bcde573605407d68551ee
Flosus <flosus.mail@gmail.com>
2015-05-18 22:51:22 +0200

    Making the powershell work

--- PackIt/Example/pack.ps1
+++ PackIt/Example/pack.ps1
@@ -6,22 +6,52 @@ Function Project($xmlProjectNode) {
 	}
 }
 Function Task($xmlTask) {
+	Write-Host $xmlTask.name
 	Foreach($node In $xmlTask.ChildNodes)
 	{
 		If($node.NodeType -Eq [Xml.XmlNodeType]::Element) {
 			$function = (Get-Command $node.Name -CommandType Function).ScriptBlock
-			Invoke-Command -scriptblock $function
+			Invoke-Command -scriptblock $function -ArgumentList $node -ea "stop"
 		}
 	}
 }
 Function copy($xmlAction) {
-	Write-Host "copy"
+	Copy-Item $xmlAction.from $xmlAction.to -ea "stop"
 }
 Function delete($xmlAction) {
-	Write-Host "delete"
+	Remove-Item $xmlAction.path -ea "stop"
 }
 Function zip($xmlAction) {
-	Write-Host "zip"
+	Add-Type -Assembly System.IO.Compression.FileSystem
+   $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+   [System.IO.Compression.ZipFile]::CreateFromDirectory($xmlAction.from,
+        $xmlAction.to, $compressionLevel, $false)
 }
-Project($xmlData.pack);
-Read-Host
+Function rename($xmlAction) {
+	Rename-Item $xmlAction.path $xmlAction.newName -ea "stop"
+}
+Function move($xmlAction) {
+	Move-Item $xmlAction.from $xmlAction.to -ea "stop"
+}
+Function createFolder($xmlAction) {
+	Try
+	{
+		New-Item $xmlAction.folder -type directory -ea "stop"
+	}
+	Catch
+	{
+		Write-Host "`tFehler beim Erstellen eines Ordners: "$_.Exception
+	}
+}
+Try
+{
+	Project($xmlData.pack);
+}
+Catch
+{
+	Write-Error $_.Exception
+}
+Finally
+{
+	Read-Host
+}
